2c4dd546644b7d82f858eaeebc06861bc1211b05
Luuk Wijnants <90309537+spongebobdespons@users.noreply.github.com>
2021-09-15 08:01:51 +0000

    second ball bouncing

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-var x;
-var v;
+var x, y, vx, vy
+var x2, y2, vx2, vy2
+
 function setup() {
 	createCanvas(500, 400);
-  v = 5;
+  
+  vx = 5;
+  vy = 5;
+  y = 200;
   x = 30;
+  vx2 = 5;
+  vy2 = 5;
+  y2 = 20;
+  x2 = 30;
 }
 
 
 
 function draw() {
 	background(225);
+
+  ellipse(x,y,50,50)
   fill('red')
-  circle(x, 130, 20);
   
-  x = x + v;
+  circle(x2,y2,25);
   
-  if (x < 0 || x > 500){ 
-    v = v * -1;
-    }
- 
- 
- }
+  x = x + vx;
+  y = y + vy;
+  
+  if(x < 0 || x > 500){
+    vx = vx * -1;
+  }
+
+  if(y < 0 || y > 400){
+    vy = vy * -1;
+  }
+  x2 = x2 + vx2;
+  y2 = y2 + vy2;
+  
+  if(x2 < 0 || x2 > 500){
+    vx2 = vx2 * -1;
+  }
+
+  if(y2 < 0 || y2 > 400){
+    vy2 = vy2 * -1;
+  }
+}
